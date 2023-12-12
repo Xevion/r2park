a991d9cdeee0cb863f8e2d35e9cf1cb595e546a1
Xevion <xevion@xevion.dev>
2023-12-12 03:10:34 -0600

    Fix StoreCode bool return, add GetCode

--- data.go
+++ data.go
@@ -6,12 +6,13 @@ import (
 
 func StoreCode(code string, location int64, member_id int) bool {
 	key := fmt.Sprintf("code:%d:%d", location, member_id)
-
-	val_exists := db.Exists(key).Val()
+	already_set := db.Exists(key).Val() == 1
 	db.Set(key, code, 0)
-	val := db.Get(code).Val()
 
-	fmt.Println(val, val_exists)
+	return already_set
+}
 
-	return false
+func GetCode(location int64, member_id int) string {
+	key := fmt.Sprintf("code:%d:%d", location, member_id)
+	return db.Get(key).Val()
 }
