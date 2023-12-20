f87e6d31fa1602f87fa52a45dfb4931a218e814f
Xevion <xevion@xevion.dev>
2023-12-20 07:55:49 -0600

    Add timedmap mod, RegisterContext struc type

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/redis/go-redis/v9 v9.3.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/zekroTJA/timedmap v1.5.2 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
--- types.go
+++ types.go
@@ -12,6 +12,14 @@ type Field struct {
 	id   string // The id of the field
 }
 
+// RegisterContext is used to store the contextual information used during registration.
+type RegisterContext struct {
+	hiddenKeys       []string // The form inputs that are hidden & unused
+	requiredFormKeys []string // The form inputs that arne't hidden - required to submit the form
+	propertyId       uint     // The property ID involved with the request
+	residentId       uint     // The resident ID involved with the request
+}
+
 const (
 	GuestCodeRequired    = iota
 	GuestCodeNotRequired = iota
